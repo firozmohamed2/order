edf68d8289f5fed3dcd9d6bcdd76d9ac695fc595
Firoz Mohamed <rizwanmuhammed1947@gmail.com>
2024-11-14 15:17:48 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/categories">Categories</Link></li>
+          <li><Link to="/profile">Profile</Link></li>
+          <li><Link to="/orders">Orders</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
