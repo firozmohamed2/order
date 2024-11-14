9501990b4846844f58937094e2128a6046f2fee3
Firoz Mohamed <rizwanmuhammed1947@gmail.com>
2024-11-14 15:22:17 +0530

    Create AboutPage.js

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We are a food delivery service committed to delivering fresh, delicious meals every day. Our menu is curated to provide a variety of options for every meal of the day.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
